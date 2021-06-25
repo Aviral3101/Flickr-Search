2830174128dc53242ba47bcaed94d94eb265309c
Aviral <aviral1234srivastava@gmail.com>
2021-06-25 16:35:42 +0530

    Hi Thanks for contribution

---
+++ src/Firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAExkbhrxtgumbbDVlnqWLC053skpUFn9s",
+    authDomain: "flickr-seacrh.firebaseapp.com",
+    projectId: "flickr-seacrh",
+    storageBucket: "flickr-seacrh.appspot.com",
+    messagingSenderId: "481766576217",
+    appId: "1:481766576217:web:dd9cdb364d66d9947aa148"
+  };
